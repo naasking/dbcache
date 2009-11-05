136232a84306e5bcfe2b22a26688e3fc7b57e374
smagi <none@none>
2009-11-05 05:47:47 +0000

    -pervasive documentation -removed unnecessary FunctionName property -use indexing instead of Dictionary.Add, to avoid erroneous duplicate errors

--- DbCache/Program.cs
+++ DbCache/Program.cs
@@ -192,12 +192,15 @@ namespace DbCache
             {
                 if (!fns.ContainsKey(name))
                 {
-                    var fn = new TypFun(name, arg, returnType);
+                    var fn = new TypFun(arg, returnType);
                     arg.Functions.Add(name, fn);
                     fns[name] = fn;
                 }
                 return Fun(name);
             }
+            /// <summary>
+            /// We're generally only interested in internal types.
+            /// </summary>
             public IEnumerable<TypIntern> Types
             {
                 get
@@ -211,6 +214,9 @@ namespace DbCache
                     }
                 }
             }
+            /// <summary>
+            /// The set of functions to be generated.
+            /// </summary>
             public IEnumerable<TypFun> Functions
             {
                 get { return fns.Values; }
@@ -235,6 +241,10 @@ namespace DbCache
                     var i = FQN.LastIndexOf('.');
                     return i < 0 ? "" : FQN.Substring(0, i);
                 }
+                /// <summary>
+                /// Return the non-fully qualified name of the type.
+                /// </summary>
+                /// <returns></returns>
                 public string BaseType()
                 {
                     var i = FQN.LastIndexOf('.') + 1;
@@ -244,6 +254,12 @@ namespace DbCache
                 {
                     return FQN;
                 }
+                /// <summary>
+                /// Resolve the Typ to a ground Typ, ie. either internal or external.
+                /// </summary>
+                /// <param name="mappings"></param>
+                /// <param name="env"></param>
+                /// <returns></returns>
                 public virtual Typ Resolve(Dictionary<string, TableMapping> mappings, Env env)
                 {
                     return this;
@@ -300,15 +316,19 @@ namespace DbCache
             /// </summary>
             public sealed class TypFun
             {
-                internal TypFun(string name, Typ arg, Typ returnType)
+                internal TypFun(Typ arg, Typ returnType)
                 {
-                    FunctionName = name;
                     ArgType = arg;
                     ReturnType = returnType;
                     Cases = new Dictionary<string, string>();
                 }
-                public string FunctionName { get; private set; }
+                /// <summary>
+                /// The input type of the function.
+                /// </summary>
                 public Typ ArgType { get; private set; }
+                /// <summary>
+                /// The return type of the function.
+                /// </summary>
                 public Typ ReturnType { get; private set; }
                 /// <summary>
                 /// Maps ArgType.FieldName to a return expression.
@@ -320,9 +340,11 @@ namespace DbCache
         // Extract set of named tables into set of enums with appropriate extension methods
         // mapping primary keys to the respective rows.
         // 1. extract table mappings
-        // 2. construct SELECT query
-        // 3. extract table schema from data reader
-        // 4. output enum + static class with enum extension methods
+        // 2. construct SELECT query for each mapping
+        // 3. extract table data and metadata from data reader
+        // 4. for each mapping, construct a lazy type graph
+        // 5. compile the type graph to a set of concrete type and function definitions
+        // 6. write the concrete types and functions to a C# file
         static void Main(string[] args)
         {
             var config = File.ReadAllLines("../../config.txt");
@@ -352,23 +374,32 @@ namespace DbCache
             }
         }
         #region Output function
+        /// <summary>
+        /// Write out the generated program as a C# file.
+        /// </summary>
+        /// <param name="env"></param>
+        /// <param name="file"></param>
+        /// <param name="namespaces"></param>
         static void Output(Env env, string file, List<string> namespaces)
         {
             if (File.Exists(file)) File.Delete(file);
             using (var op = File.CreateText(file))
             {
+                // included namespaces
                 op.WriteLine("using System;");
                 foreach (var ns in namespaces) op.WriteLine("using {0};", ns);
 
                 // write out enum declaration with its stub
                 foreach (var type in env.Types)
                 {
+                    // enum namespace
                     var ns = type.Namespace();
                     if (!string.IsNullOrEmpty(ns))
                     {
                         op.WriteLine("namespace {0}", ns);
                         op.WriteLine('{');
                     }
+                    // write out enum definition
                     op.WriteLine("public enum {0}", type.BaseType());
                     op.WriteLine('{');
                     foreach (var value in type.Values)
@@ -377,21 +408,25 @@ namespace DbCache
                     }
                     op.WriteLine('}');
 
+                    // write out any column functions, if applicable
                     if (type.Functions.Count > 0)
                     {
                         op.WriteLine("public static class {0}Extensions", type.BaseType());
                         op.WriteLine('{');
-                        foreach (var fn in type.Functions.Values)
+                        foreach (var fn in type.Functions)
                         {
+                            // each function is an extension method which switches
+                            // on the enum value and maps it to another value
+
                             op.WriteLine("    public static {0} {1}(this {2} value)",
-                                         fn.ReturnType, fn.FunctionName, fn.ArgType.BaseType());
+                                         fn.Value.ReturnType, fn.Key, fn.Value.ArgType.BaseType());
                             op.WriteLine("    {");
                             op.WriteLine("        switch (value)");
                             op.WriteLine("        {");
-                            foreach (var _case in fn.Cases)
+                            foreach (var _case in fn.Value.Cases)
                             {
                                 op.WriteLine("            case {0}.{1}: return {2};",
-                                             fn.ArgType.BaseType(), _case.Key, _case.Value);
+                                             fn.Value.ArgType.BaseType(), _case.Key, _case.Value);
                             }
                             op.WriteLine("            default: throw new ArgumentException(\"Invalid {0} provided.\");",
                                          type.BaseType());
@@ -408,6 +443,12 @@ namespace DbCache
             }
         }
         #endregion
+        /// <summary>
+        /// Quote the given value as a C# expression given the actual and expected type.
+        /// </summary>
+        /// <param name="val"></param>
+        /// <param name="expected"></param>
+        /// <returns></returns>
         static string quote(object val, Type expected)
         {
             return val == null   ? "default(" + expected.FullName + ")":
@@ -415,6 +456,11 @@ namespace DbCache
                    val is bool   ? val.ToString().ToLower():
                                    val.ToString();
         }
+        /// <summary>
+        /// Normalize the given string as a valid C# identifier.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         static string normalize(this string name)
         {
             var sb = new StringBuilder();
@@ -431,6 +477,14 @@ namespace DbCache
             }
             return sb.ToString();
         }
+        /// <summary>
+        /// Perform a value or template substitution, depending on the given args.
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <param name="col"></param>
+        /// <param name="value"></param>
+        /// <param name="expectedType"></param>
+        /// <returns></returns>
         static string substitute(string expression, string col, string value, Env.Typ expectedType)
         {
             return string.IsNullOrEmpty(expression) ? value:
@@ -457,13 +511,13 @@ namespace DbCache
         {
             // load column definitions, ie. names and types, starting with
             // PK and name column
-            data.Columns.Add(table.PK, reader.GetFieldType(0));
-            data.Columns.Add(table.Name, reader.GetFieldType(1));
+            data.Columns[table.PK] = reader.GetFieldType(0);
+            data.Columns[table.Name] = reader.GetFieldType(1);
             table.Data = data;
             foreach (var col in table.Columns)
             {
                 var i = reader.GetOrdinal(col.Key);
-                data.Columns.Add(col.Key, reader.GetFieldType(i));
+                data.Columns[col.Key] = reader.GetFieldType(i);
             }
         }
         static void BindRows(TableData data, TableMapping table, DbDataReader reader)
@@ -472,17 +526,23 @@ namespace DbCache
             foreach (IDataRecord drow in reader)
             {
                 var row = new RowData();
-                    row.Cells.Add(table.PK, drow.GetValue(0));
-                    row.Cells.Add(table.Name, drow.GetValue(1));
+                    row.Cells[table.PK] = drow.GetValue(0);
+                    row.Cells[table.Name] = drow.GetValue(1);
                 data.Rows.Add(row);
                 foreach (var col in table.Columns)
                 {
                     var i = drow.GetOrdinal(col.Key);
-                    row.Cells.Add(col.Key, drow.GetValue(i));
+                    row.Cells[col.Key] = drow.GetValue(i);
                 }
             }
         }
         #endregion
+        /// <summary>
+        /// Compile the given table with the given environment and table mappings.
+        /// </summary>
+        /// <param name="table"></param>
+        /// <param name="mappings"></param>
+        /// <param name="env"></param>
         static void Compile(TableMapping table, Dictionary<string, TableMapping> mappings, Env env)
         {
             // declare top-level type
@@ -497,10 +557,18 @@ namespace DbCache
                 var returnType = string.IsNullOrEmpty(col.Value.ReturnType)
                                ? table.Data.Columns[col.Key].FullName
                                : col.Value.ReturnType;
+                // by default, ReturnType is TypVar, ie. an unresolved type
+                // the concrete type is resolved on-demand
                 env.Fun(col.Value.Function, type, env.Var(returnType));
             }
             Functions(table, mappings, env);
         }
+        /// <summary>
+        /// Compile the given table's functions with the given environment and table mappings.
+        /// </summary>
+        /// <param name="table"></param>
+        /// <param name="mappings"></param>
+        /// <param name="env"></param>
         static void Functions(TableMapping table, Dictionary<string, TableMapping> mappings, Env env)
         {
             var type = env.Define(table.EnumFQN);
@@ -543,15 +611,27 @@ namespace DbCache
                         // substitute escaped field value for quoted column name
                         exp = substitute(col.Value.Expression, col.Key, fk, returnedType);
                     }
-                    fn.Cases.Add(pkname, exp);
+                    fn.Cases[pkname] = exp;
                 }
             }
         }
         #region Parsing functions
+        /// <summary>
+        /// Convenient short hand for splitting strings.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="input"></param>
+        /// <returns></returns>
         static string[] split(string token, string input)
         {
             return input.Split(StringSplitOptions.RemoveEmptyEntries, token);
         }
+        /// <summary>
+        /// Database connection factory.
+        /// </summary>
+        /// <param name="connType"></param>
+        /// <param name="db"></param>
+        /// <returns></returns>
         static DbConnection GetConn(string connType, string db)
         {
             switch (connType)
@@ -560,6 +640,13 @@ namespace DbCache
                 default:          return new OleDbConnection(db);
             }
         }
+        /// <summary>
+        /// Search the given config values by key, and return otherwise if key not found.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <param name="key"></param>
+        /// <param name="otherwise"></param>
+        /// <returns></returns>
         static string FindByKey(this string[] args, string key, string otherwise)
         {
             var v = from a in args
@@ -571,14 +658,27 @@ namespace DbCache
             if (result == null) throw new ArgumentException("Missing " + key);
             return result;
         }
+        /// <summary>
+        /// Throw a parse error with line number info.
+        /// </summary>
+        /// <param name="cond"></param>
+        /// <param name="err"></param>
+        /// <param name="line"></param>
         static void error(bool cond, string err, int line)
         {
             if (cond) throw new ArgumentException(string.Format("ERROR: line {0}, {1}.", line+1, err));
         }
+        /// <summary>
+        /// Parse the mapping files and command-line arguments.
+        /// </summary>
+        /// <param name="config"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
         static Config Parse(string[] config, string[] args)
         {
             var map = new Dictionary<string, TableMapping>();
             var cfg = new Config { Namespaces = new List<string>(), Mappings = map };
+            // parse command-line arguments
             foreach (var a in args)
             {
                 if (a.StartsWith("/out:"))
@@ -586,10 +686,12 @@ namespace DbCache
                     cfg.OutputFile = a.Substring("/out:".Length);
                 }
             }
+            // parse mapping file
             for (var i = 0; i < config.Length; ++i)
             {
                 if (config[i].StartsWith("::database"))
                 {
+                    // declare the database type and connection string
                     var line = config[i].Split('=');
                     error(line.Length < 2, "Improper ::database declaration", i);
                     var dbType = line[1].Trim();
@@ -599,6 +701,7 @@ namespace DbCache
                 }
                 else if (config[i].StartsWith("::using"))
                 {
+                    // included namespaces in output file
                     var line = config[i].Split('=');
                     error(line.Length < 2, "Improper ::using declaration", i);
                     var ns = line[1].Trim();
@@ -606,6 +709,7 @@ namespace DbCache
                 }
                 else if (config[i].StartsWith("::table"))
                 {
+                    // extract top-level table info, ie. pk, enum name, etc.
                     var table = split("::", config[i]);
                     error(table.Length < 4, "::table requires table, enum, pk, and name specified.", i);
                     var tname = table.FindByKey("table", null);
@@ -617,22 +721,24 @@ namespace DbCache
                         Name = table.FindByKey("name", null),
                         Columns = new Dictionary<string, ColumnMapping>(),
                     };
+                    // extract column info from mapping file
                     for (++i; i < config.Length && !config[i].StartsWith("::"); ++i)
                     {
-                        //if (config[i].StartsWith("--")) continue; // skip comments
                         error(string.IsNullOrEmpty(config[i]), "Missing column information for table " + tname, i);
                         var column = split("::", config[i]);
                         var name = column[0].Trim();
-                        tableMap.Columns.Add(name, new ColumnMapping
+                        tableMap.Columns[name] = new ColumnMapping
                         {
                             Table = tableMap,
                             Function = column.FindByKey("function", name),
                             Expression = column.FindByKey("expression", ""),
                             ReturnType = column.FindByKey("returnType", ""),
-                        });
+                        };
                     }
-                    --i; // undo the last increment after processing columns
-                    map.Add(tableMap.EnumFQN, tableMap);
+                    // undo the last increment after processing columns, since
+                    // outer loop increments i again
+                    --i;
+                    map[tableMap.EnumFQN] = tableMap;
                 }
             }
             return cfg;
